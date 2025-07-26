23c44558255bc49870f0986b4014845f7bb2f01c
TheGift20obj <djadunov@gmail.com>
2025-07-26 08:31:18 +0200

    Update lib.rs

--- src/project_chatgpt_backend/src/lib.rs
+++ src/project_chatgpt_backend/src/lib.rs
@@ -61,8 +61,7 @@ pub struct HttpRequest {
 
 #[update]
 async fn chat(prompt: String) -> String {
-    let openai_api_key = std::env::var("OPENAI_API_KEY").unwrap_or_else(|_| "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".to_string());
-    //second key: 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
+    let openai_api_key = std::env::var("OPENAI_API_KEY").unwrap_or_else(|_| "NONE".to_string());
     let body = json!({
         "model": "gpt-4o-mini",
         "messages": [{
